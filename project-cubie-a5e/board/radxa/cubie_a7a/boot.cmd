794da76623fdfe462a3a8b3d8c3b51ffadac9623
Tim Michals <tcmichals@yahoo.com>
2026-08-22 22:55:00 -0500

    fix(a7a): add rw to bootargs, auto-login root shell inittab, and non-blocking interfaces

--- project-cubie-a5e/board/radxa/cubie_a7a/boot.cmd
+++ project-cubie-a5e/board/radxa/cubie_a7a/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttyS0,115200 earlycon=uart8250,mmio32,0x02500000 root=/dev/mmcblk0p2 rootwait panic=10 isolcpus=7 nohz_full=7 rcu_nocbs=7 loglevel=8
+setenv bootargs console=ttyS0,115200 earlycon=uart8250,mmio32,0x02500000 root=/dev/mmcblk0p2 rootwait rw panic=10 isolcpus=7 nohz_full=7 rcu_nocbs=7 loglevel=8
 setenv kernel_addr_r 0x40200000
 setenv fdt_addr_r 0x4fa00000
 
